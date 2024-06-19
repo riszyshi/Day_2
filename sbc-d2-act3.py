acc34fed428aaccd2c4d72082a35219abc088d15
riszyshi <krista.mikah.agunod@aclcbutuan.edu.ph>
2024-06-19 23:52:58 +0800

    assignment

---
+++ sbc-d2-act3.py
@@ -0,0 +1,18 @@
+from random import randint
+
+print("WELCOME TO SWERTRES!\n")
+
+user_bet = input("Place your 3-digit bet: ")
+
+print(f"Your 3-digit bet: {user_bet}")
+
+winning_num = ''.join([str(randint(0, 9)) for _ in range(3)])
+
+print(f"The winning number is: {winning_num}")
+
+if user_bet == winning_num:
+    print("You Won!")
+elif sorted(user_bet) == sorted(winning_num):
+    print("You partially won!")
+else:
+    print("Sorry, better luck next time!")
